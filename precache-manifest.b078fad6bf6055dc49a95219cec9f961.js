f6c98cb52acf25307b2e81d178139c5d575c1834
the-gaffer-5 <prescottblackler@gmail.com>
2020-03-19 23:28:49 -0400

    deploy

---
+++ precache-manifest.b078fad6bf6055dc49a95219cec9f961.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a3a07f7c8814df3ffe0fa01fdaec2c",
+    "url": "/volumental/index.html"
+  },
+  {
+    "revision": "45f9fed8f8c95e55fd1c",
+    "url": "/volumental/static/css/main.511c8e9a.chunk.css"
+  },
+  {
+    "revision": "864ad01b900e59186788",
+    "url": "/volumental/static/js/2.763f115e.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/volumental/static/js/2.763f115e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f9fed8f8c95e55fd1c",
+    "url": "/volumental/static/js/main.c1c67e11.chunk.js"
+  },
+  {
+    "revision": "511d71d3bba2360e7884",
+    "url": "/volumental/static/js/runtime-main.c93c2540.js"
+  },
+  {
+    "revision": "a6bf901829858276f852938efb5f2ff0",
+    "url": "/volumental/static/media/5c9c9a6b202d4367c4131a0b_logotype.a6bf9018.svg"
+  },
+  {
+    "revision": "af71c625349936b86dc006efef5e280f",
+    "url": "/volumental/static/media/error-shoes.af71c625.jpg"
+  },
+  {
+    "revision": "4eefd8902592105711b09a8879cb0d92",
+    "url": "/volumental/static/media/login.4eefd890.jpg"
+  }
+]);
